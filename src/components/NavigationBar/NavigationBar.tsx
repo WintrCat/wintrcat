71952827e74cc6f70c3a68e7c49d2077f57a34a9
WintrCat <wintrcat.wilson@gmail.com>
2024-04-26 00:04:52 +0100

    feat: add social media icons to navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import * as styles from "./NavigationBar.module.css";
 
@@ -10,9 +10,25 @@ function NavigationBar() {
         </div>
 
         <div className={styles.navigationLinks}>
-            <a className={styles.navigationLink} href="">👋🏻 About Me</a>
-            <a className={styles.navigationLink} href="">🚅 Projects</a>
-            <a className={styles.navigationLink} href="">✉️ Contact</a>
+            <div className={styles.socialsContainer}>
+                <a className={styles.socialIcon} href="https://github.com/wintrcat">
+                    <img src="/img/github.svg" alt="GitHub" height="25" />
+                </a>
+                <a className={styles.socialIcon} href="https://youtube.com/@wintrcat">
+                    <img src="/img/youtube.svg" alt="YouTube" height="25" />
+                </a>
+                <a className={styles.socialIcon} href="https://instagram.com/@wintrcat">
+                    <img src="/img/instagram.svg" alt="Instagram" height="25" />
+                </a>
+            </div>
+
+            <div className={styles.separator}></div>
+
+            <div className={styles.pageSectionsContainer}>
+                <a className={styles.pageSectionLink} href="">👋🏻 About Me</a>
+                <a className={styles.pageSectionLink} href="">🚅 Projects</a>
+                <a className={styles.pageSectionLink} href="">✉️ Contact</a>
+            </div>
         </div>
     </div>;
 }
